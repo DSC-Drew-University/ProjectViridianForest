0a1f3fae644a468ec29580bb46991befacf1ac80
Katlego <katlego@carry1st.com>
2019-12-02 18:36:17 -0500

    Initial Commit

---
+++ android/app/src/main/kotlin/com/drew/dsc/dsc_viridian_forest/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.drew.dsc.dsc_viridian_forest
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
